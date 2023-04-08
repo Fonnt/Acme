326a884cc2a66f42530ce76bc935754b6e4d9f9d
Fonnt <dballesterfont@gmail.com>
2023-04-08 21:37:02 +0200

    [db] Updated render with createRoot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+import * as ReactDOMClient from 'react-dom/client';
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.css'
 import { App } from './App'
+import './styles/reset.css'
+import './styles/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root');
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the root.
+root.render(<App tab="home" />);
+
+// During an update, there's no need to pass the container again.
+root.render(<App tab="profile" />);
