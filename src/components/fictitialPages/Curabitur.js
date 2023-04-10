b0b6e480aa167e0e04ad0a64873554fef2fbfd9a
Fonnt <dballesterfont@gmail.com>
2023-04-10 21:36:33 +0200

    [db] Created extra subpages with basic content

--- src/components/fictitialPages/Curabitur.js
+++ src/components/fictitialPages/Curabitur.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import content from 'utils/content.json'
 
 const Curabitur = () => {
   return (
-    <div>Curabitur</div>
+    <section className="main-wrapper">
+      <div className="services-info-wrapper">
+        <div className="services-info-content">
+          <h1>{content.services[0].title}</h1>
+          <p>
+            {content.services[0].info}
+          </p>
+          <img
+            className="services-info-img"
+            src={content.services[0].image}
+            alt="interior" />
+        </div>
+      </div>
+    </section>
   )
 }
 
